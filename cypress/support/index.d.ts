56cba23ab54082167777ae37bc13af2fae81ab4e
Brian Ouz <bouzomgi@gmail.com>
2025-08-04 19:20:31 -0400

    fix cypress

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -4,6 +4,11 @@ declare namespace Cypress {
   interface Chainable<Subject = any> {
     areUserDetailsSetInLocalStorage(): Chainable<any>
     login(options?: loginOptions): Chainable<any>
+    triggerSocketEvent(event: any): Chainable<any>
+    mockWebSocket(
+      url: string,
+      options?: { useDefaultWebSocket?: boolean }
+    ): Chainable<Subject>
   }
 
   interface loginOptions {
