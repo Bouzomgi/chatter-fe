6f8270bf6e8370df280daf60378fe504d09f2269
Brian Ouz <bouzomgi@gmail.com>
2025-08-04 21:24:33 -0400

    upload cypress screenshots

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,8 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export default defineConfig({
+  screenshotsFolder: 'cypress/screenshots',
+  screenshotOnRunFailure: true,
   e2e: {
     baseUrl: process.env.CYPRESS_BASE_URL,
     setupNodeEvents(on, config) {
